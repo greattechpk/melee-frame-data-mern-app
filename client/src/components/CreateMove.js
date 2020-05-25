05aac9da0050ef6e1e564406930cdcced13c4a74
greattechpk <great.techpk@gmail.com>
2020-05-25 17:34:04 -0400

    added on change functionality to text inputs and text area begining to test on submit

--- client/src/components/CreateMove.js
+++ client/src/components/CreateMove.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 export default class CreateMove extends Component {
@@ -29,6 +30,12 @@ export default class CreateMove extends Component {
         this.setState(newState)
         console.log(this.state)
     }
+    onChangeText = evt =>{
+        const newState = {...this.state}
+        newState.newMove[evt.target.name]=evt.target.value
+        this.setState(newState)
+        console.log(this.state.newMove[evt.target.name])
+    }
 
     onSubmit = async (evt) => {
         evt.preventDefault()
@@ -45,21 +52,24 @@ export default class CreateMove extends Component {
     }
 
     render() {
+        if (this.state.redirect) {
+            return (<Redirect to={`/admin-character/${this.state.characterId}`} />)
+        }
         return (
             <div>
                 <h2>Create a move for {this.props.name}</h2>
                 <form id='move-form' onSubmit={this.onSubmit}>
                     <div className='form-group'>
                         <label htmlFor='name'>Move Name:</label>
-                        <input type="text" name="name"></input>
+                        <input type="text" name="name" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='gif'>.GIF url:</label>
-                        <input type="text" name="gif"></input>
+                        <input type="text" name="gif" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='notes'>Notes:</label>
-                        <textarea name='notes' form='move-form'>Enter notes here...</textarea>
+                        <textarea name='notes' form='move-form' onChange={this.onChangeText}>Enter notes here...</textarea>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='type'>Type:</label>
@@ -73,35 +83,35 @@ export default class CreateMove extends Component {
                     </div>
                     <div className='form-group'>
                         <label htmlFor='totalFrames'>Total Frames:</label>
-                        <input type="number" name="totalFrames"></input>
+                        <input type="number" name="totalFrames" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='activeHit'>Hit Boxes Active For:</label>
-                        <input type="text" name="activeHit"></input>
+                        <input type="text" name="activeHit" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='startUpFrames'>Frames Before Hit Boxes:</label>
-                        <input type="text" name="startUpFrames"></input>
+                        <input type="text" name="startUpFrames" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='endFrames'>Frames After Hit Boxes:</label>
-                        <input type="text" name="endFrames"></input>
+                        <input type="text" name="endFrames"  onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='iasa'>IASA Frame:</label>
-                        <input type="number" name="iasa"></input>
+                        <input type="number" name="iasa" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='autoCancel'>Auto Cancel:</label>
-                        <input type="text" name="autoCancel"></input>
+                        <input type="text" name="autoCancel" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='landLag'>Landing Lag:</label>
-                        <input type="number" name="landLag"></input>
+                        <input type="number" name="landLag" onChange={this.onChangeText}></input>
                     </div>
                     <div className='form-group'>
                         <label htmlFor='lCanceled'>L Cancel Landing Lag:</label>
-                        <input type="number" name="lCanceled"></input>
+                        <input type="number" name="lCanceled" onChange={this.onChangeText}></input>
                     </div>
                     <input type="submit" value="Create Move" />
                 </form>
